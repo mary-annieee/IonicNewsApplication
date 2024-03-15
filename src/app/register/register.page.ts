f10ff3a4f2f417a8cae522b0f85ec51d1bac95d6
Mary Annie S <mary.annie@sifycorp.com>
2024-03-15 15:19:02 +0530

    register commit

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  name: string="";
+  age: number=0;
+  dob: string="";
+  place: string="";
+  location: string="";
 
   constructor() { }
 
   ngOnInit() {
+  }
+  register(){
+    
   }
 
 }
